c8996374e299e5b05522d3f315da4944a4601fc5
bala-amypo <bala@amypo.in>
2025-12-17 08:11:27 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,1 +1,8 @@
-public class Gargage
+public class Garage{
+    private long id;
+    private String gargaeName;
+    private String address;
+    private String contactNumber;
+    private Boolean active;
+    
+}
