46e47b196ccccbdbc6de8beb43c87ec8dda6b18e
bala-amypo <bala@amypo.in>
2025-12-17 08:26:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,8 +1,52 @@
-public class Garage{
+package com.example.demo.model;
+
+public class Garage {
     private long id;
-    private String gargaeName;
+    private String garageName;
     private String address;
     private String contactNumber;
     private Boolean active;
+    
+    public Garage(long id, String garageName, String address, String contactNumber, Boolean active) {
+        this.id = id;
+        this.garageName = garageName;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.active = active;
+    }
+    public Garage(){
+
+    }
+    public long getId() {
+        return id;
+    }
+    public String getGarageName() {
+        return garageName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getContactNumber() {
+        return contactNumber;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setGarageName(String garageName) {
+        this.garageName = garageName;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    
     
 }
