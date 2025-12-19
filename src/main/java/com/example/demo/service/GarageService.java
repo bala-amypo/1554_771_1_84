fcdebe030c3e4a9b99a1e38abde59691148245bb
bala-amypo <bala@amypo.in>
2025-12-19 05:00:52 +0000

    User commit

--- src/main/java/com/example/demo/service/GarageService.java
+++ src/main/java/com/example/demo/service/GarageService.java
@@ -1,1 +1,9 @@
 
+package com.example.demo.service;
+
+import com.example.demo.model.Garage;
+
+public interface GarageService {
+    Garage createGarage(Garage garage);
+}
+                                        
