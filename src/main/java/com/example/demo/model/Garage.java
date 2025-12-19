45b7f7de120c390e26965c9067eae2429a25f6d1
bala-amypo <bala@amypo.in>
2025-12-19 06:14:11 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,53 +1,58 @@
- package com.example.demo.model;
+package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "garages")
 public class Garage {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String garageName;
     private String address;
-    private String contactNumber;
     private Boolean active;
-    
-    public Garage(long id, String garageName, String address, String contactNumber, Boolean active) {
+
+    public Garage() {}
+
+
+    public Garage(Long id, String garageName, String address, Boolean active) {
         this.id = id;
         this.garageName = garageName;
         this.address = address;
-        this.contactNumber = contactNumber;
         this.active = active;
     }
-    public Garage(){
 
-    }
-    public long getId() {
+    public Long getId() {
         return id;
     }
+
     public String getGarageName() {
         return garageName;
     }
+
     public String getAddress() {
         return address;
     }
-    public String getContactNumber() {
-        return contactNumber;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public void setGarageName(String garageName) {
-        this.garageName = garageName;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
+
     public Boolean getActive() {
         return active;
     }
-    
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setGarageName(String garageName) {
+        this.garageName = garageName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
     
 }
-                                                                                                                                    
