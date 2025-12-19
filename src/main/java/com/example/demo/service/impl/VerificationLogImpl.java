e77ce41a25fcd258bf2b24357eb0f57357c802c2
bala-amypo <bala@amypo.in>
2025-12-19 05:10:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/VerificationLogImpl.java
@@ -0,0 +1,30 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.EntityNotFoundException;
+import com.example.demo.model.VerificationLog;
+import com.example.demo.model.ServiceEntry;
+import com.example.demo.repository.VerificationLogRepository;
+import com.example.demo.repository.ServiceEntryRepository;
+import com.example.demo.service.VerificationLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+public class VerificationLogServiceImpl implements VerificationLogService {
+
+    @Autowired
+    private VerificationLogRepository verificationLogRepository;
+    @Autowired
+    private ServiceEntryRepository serviceEntryRepository;
+
+    @Override
+    public VerificationLog createLog(VerificationLog log) {
+        ServiceEntry entry = serviceEntryRepository.findById(log.getServiceEntry().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Service entry not found"));
+
+        log.setServiceEntry(entry);
+        log.setVerifiedAt(LocalDateTime.now());
+        return verificationLogRepository.save(log);
+    }
+}
