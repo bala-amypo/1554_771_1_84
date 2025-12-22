44d7701aa0f93614421b3b4c235f0e20f5f557b3
bala-amypo <bala@amypo.in>
2025-12-22 09:29:49 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -4,76 +4,82 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "service_entries")
 public class ServiceEntry {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    private Vehicle vehicle;
-    @ManyToOne
-    private Garage garage;
-    private String serviceType;
-    private LocalDate serviceDate;
-    private Integer odometerReading;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle; // Many ServiceEntries belong to one Vehicle
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "garage_id")
+    private Garage garage; // Many ServiceEntries belong to one Garage
+
+    private Long odometerReading; // Odometer reading at the time of service
+    private LocalDate serviceDate; // Date of service
+
+    // Constructor, Getters, Setters, equals(), hashCode(), toString() methods
 
     public ServiceEntry() {}
 
-    public ServiceEntry(Vehicle vehicle, Garage garage, String serviceType, LocalDate serviceDate, Integer odometerReading) {
+    public ServiceEntry(Vehicle vehicle, Garage garage, Long odometerReading, LocalDate serviceDate) {
         this.vehicle = vehicle;
         this.garage = garage;
-        this.serviceType = serviceType;
-        this.serviceDate = serviceDate;
         this.odometerReading = odometerReading;
+        this.serviceDate = serviceDate;
     }
 
     public Long getId() {
         return id;
     }
 
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
-
-    public Garage getGarage() {
-        return garage;
-    }
-
-    public String getServiceType() {
-        return serviceType;
-    }
-
-    public LocalDate getServiceDate() {
-        return serviceDate;
-    }
-
-    public Integer getOdometerReading() {
-        return odometerReading;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
     }
 
     public void setVehicle(Vehicle vehicle) {
         this.vehicle = vehicle;
     }
 
+    public Garage getGarage() {
+        return garage;
+    }
+
     public void setGarage(Garage garage) {
         this.garage = garage;
     }
 
-    public void setServiceType(String serviceType) {
-        this.serviceType = serviceType;
+    public Long getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(Long odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public LocalDate getServiceDate() {
+        return serviceDate;
     }
 
     public void setServiceDate(LocalDate serviceDate) {
         this.serviceDate = serviceDate;
     }
 
-    public void setOdometerReading(Integer odometerReading) {
-        this.odometerReading = odometerReading;
+    @Override
+    public String toString() {
+        return "ServiceEntry{" +
+                "id=" + id +
+                ", vehicle=" + vehicle +
+                ", garage=" + garage +
+                ", odometerReading=" + odometerReading +
+                ", serviceDate=" + serviceDate +
+                '}';
     }
-
-   
 }
