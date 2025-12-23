042a5f5156b131bf4dc82e4f78c9b30d898dff63
bala-amypo <bala@amypo.in>
2025-12-23 03:47:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/ServicePartController.java
+++ src/main/java/com/example/demo/controller/ServicePartController.java
@@ -2,20 +2,20 @@ package com.example.demo.controller;
 
 import com.example.demo.model.ServicePart;
 import com.example.demo.service.ServicePartService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/service-parts")
 public class ServicePartController {
 
-    @Autowired
-    private ServicePartService servicePartService;
+    private final ServicePartService servicePartService;
+
+    public ServicePartController(ServicePartService servicePartService) {
+        this.servicePartService = servicePartService;
+    }
 
     @PostMapping
-    public ResponseEntity<ServicePart> createPart(@RequestBody ServicePart part) {
-        ServicePart saved = servicePartService.createPart(part);
-        return ResponseEntity.ok(saved);
+    public ServicePart createServicePart(@RequestBody ServicePart servicePart) {
+        return servicePartService.createPart(servicePart);
     }
 }
